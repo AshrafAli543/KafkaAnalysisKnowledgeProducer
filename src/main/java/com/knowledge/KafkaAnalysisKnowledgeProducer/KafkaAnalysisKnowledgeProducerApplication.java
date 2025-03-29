24d3c39993b3d82c3f4d09daee60b3fefb9a25cc
Ashraf Ali.Shaik <ashrafalis711@gmail.com>
2025-03-29 17:02:43 +0530

    initial commit of maven project to remote

---
+++ src/main/java/com/knowledge/KafkaAnalysisKnowledgeProducer/KafkaAnalysisKnowledgeProducerApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge.KafkaAnalysisKnowledgeProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAnalysisKnowledgeProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAnalysisKnowledgeProducerApplication.class, args);
+	}
+
+}
